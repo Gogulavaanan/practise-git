c849a3aa4b1389dfbdcba0132e42e372d6e52a2e
Gogulavaanan <108971001+Gogulavaanan@users.noreply.github.com>
2022-07-10 12:48:59 +0530

    java

---
+++ java_first class/src/org/firstclass/java/second.java
@@ -0,0 +1,35 @@
+package org.firstclass.java;
+
+public class second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("welcome");
+		//System.out.print("wel");
+		//System.out.println("come");
+	//	System.out.println("12"+"3");
+		//System.out.println(12+3);
+		//System.out.println("12+3");
+		//System.out.println(3+"5");
+	//	System.out.println(52/5);
+		//System.out.println(52%5);
+		//System.out.println(52*5);
+		//String  a ="java";
+				//String b = "Besant";
+				//System.out.println(a+b);
+			//	System.out.println(a+" "+b);
+		//var  a ="java";
+		//var b = "Besant";
+		//System.out.println(a+b);
+	//	System.out.println(a+" "+b);
+		
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		
+		
+				
+		
+	}
+
+}
